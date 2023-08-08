b768f244c7c91d56f53924d87737918bd1d27a99
sangmin <xectler@gmail.com>
2023-08-08 12:20:26 +0900

    헤더 만드는 중

---
+++ leagueOfLegend/js/main.js
@@ -0,0 +1,5 @@
+const headerLogo = document.querySelector('.header .logo')
+
+headerLogo.addEventListener('click', e => {
+    console.log('headerLogo 클릭')
+})
--- readme.md
+++ readme.md
@@ -1,3 +1,2 @@
 ## 웹사이트 클론
 - [NEXON](https://spontaneous-bienenstitch-474013.netlify.app/)
-    - 반응형 X
