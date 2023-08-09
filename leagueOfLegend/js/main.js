d3327fe5ffb547637e1ab830b93ab75d8fbbae29
sangmin <xectler@gmail.com>
2023-08-09 11:23:40 +0900

    드랍다운 메뉴 + 배너 만드는 중...

--- leagueOfLegend/js/main.js
+++ leagueOfLegend/js/main.js
@@ -1,5 +1,24 @@
 const headerLogo = document.querySelector('.header .logo')
+const backdrop = document.querySelector('.backdrop')
+const headerNavMenu = document.querySelector('.header-nav-menu')
+const headerNavLogo = headerNavMenu.querySelector('.logo > a')
+const headerNavClose = headerNavMenu.querySelector('.close > a')
 
 headerLogo.addEventListener('click', e => {
-    console.log('headerLogo 클릭')
+    headerNavMenu.classList.add('active')
+    backdrop.classList.add('active')
+    document.documentElement.style.overflow = 'hidden'
 })
+
+headerNavLogo.addEventListener('click', e => {
+    headerNavMenu.classList.remove('active')
+    backdrop.classList.remove('active')
+    document.documentElement.style.overflow = 'initial'
+})
+
+headerNavClose.addEventListener('click', e => {
+    headerNavMenu.classList.remove('active')
+    backdrop.classList.remove('active')
+    document.documentElement.style.overflow = 'initial'
+})
+
--- leagueOfLegend/readme.md
+++ leagueOfLegend/readme.md
@@ -1,1 +1,4 @@
 - RixSGO 폰트가 유로라서 헤더는 맑은 고딕을 사용
+
+- [break-inside](https://developer.mozilla.org/en-US/docs/Web/CSS/break-inside)
+- [다단 레이아웃 columns](https://developer.mozilla.org/ko/docs/Web/CSS/CSS_multicol_layout/Using_multicol_layouts)
